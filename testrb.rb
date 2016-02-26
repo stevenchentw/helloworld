2bbd12808025ec5ba5eba5f9876f75c4a85441a0
stevenchentw <sadz9999@hotmail.com>
2016-02-26 15:07:31 +0800

    QQ

--- testrb.rb
+++ testrb.rb
@@ -1,4 +1,20 @@
-puts "開妹妹？"
+puts "誰想要開妹妹？"
 puts "plz answer"
-x = gets()
-puts x + "let's go~~~"
+
+begin
+	begin
+  puts (" 1. Kahoo 2. Steven 3. Hugo ")
+  x = gets().to_i
+  end while ![1,2,3].include?(x)
+
+  if x == 1
+  	puts ("Kahoo說：")
+    puts ("來香港桑拿啦~ 很好玩的")
+  elsif x == 2
+  	puts ("Steven說：")
+    puts ("台灣很好玩~ 可以聊一下")
+  else 
+  	puts ("Hugo說：")
+    puts ("年輕的妹妹我很多, 你要哪一個？")
+  end
+end
