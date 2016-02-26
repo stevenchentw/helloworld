408f36eadc57836fe9cfe19e93116da87aac5eec
stevenchentw <sadz9999@hotmail.com>
2016-02-26 14:04:11 +0800

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -1,3 +1,9 @@
 puts ("Hello World")
 puts ("Hello World")
 puts ("Hello World")
+
+puts ("將本地目錄初始化變成 git 支援的目錄，指令是 init  ") 
+puts ("將檔案遞交進 Git repository，指令是 commit  ")
+puts ("設定 Git repository 的遠端伺服器位置，指令是 remote  ")
+puts ("第一次將本地 Git repository 推送上 github，指令是 push ")
+puts ("請新增一些檔案，再做一次遞交並推送上 github，指令是 add ")
