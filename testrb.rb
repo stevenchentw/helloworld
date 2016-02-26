b2fd393ba6540b611722bbb684d559f580963b92
Ka Ho <KaHo@Ka-Hos-MacBook-Pro.local>
2016-02-26 15:15:44 +0800

    added for max

--- testrb.rb
+++ testrb.rb
@@ -4,7 +4,7 @@ begin
 	begin
   puts (" 1. Kahoo 2. Steven 3. Hugo ")
   x = gets().to_i
-  end while ![1,2,3].include?(x)
+  end while ![1,2,3,4].include?(x)
 
   if x == 1
   	puts ("Kahoo說：")
@@ -12,8 +12,11 @@ begin
   elsif x == 2
   	puts ("Steven說：")
     puts ("台灣很好玩~ 可以聊一下")
-  else 
+  elseif x == 3 
   	puts ("Hugo說：")
     puts ("年輕的妹妹我很多, 你要哪一個？")
+  elseif x == 4
+	puts ("Max說：")
+    puts ("你們真無聊")
   end
 end
